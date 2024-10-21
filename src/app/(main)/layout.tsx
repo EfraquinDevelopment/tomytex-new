773943dfa5d87e53aff67a31c797c1e2245c002d
kevinefraim <kevinefraim02@gmail.com>
2024-10-20 21:16:06 -0300

    seo

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,14 +6,14 @@ import "../globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "TOMYTEX | Telas Deportivas en Buenos Aires, Flores | Local de Telas",
+  title: "TOMYTEX | Telas Deportivas | Local de Telas",
   description:
-    "Descubre TOMYTEX, el local líder en telas deportivas en Buenos Aires, Flores. Telas de alta calidad para indumentaria deportiva, fitness y más. ¡Visítanos hoy!",
+    "Descubre TOMYTEX, el local líder en telas deportivas. Telas de alta calidad para indumentaria deportiva, fitness y más. ¡Visítanos hoy!",
   keywords:
     "TOMYTEX, telas deportivas, Buenos Aires, Flores, telas para indumentaria deportiva, telas de alta calidad, telas fitness, venta de telas deportivas",
   robots: "index, follow",
   openGraph: {
-    title: "TOMYTEX | Telas Deportivas en Buenos Aires, Flores",
+    title: "TOMYTEX | Telas Deportivas",
     description:
       "TOMYTEX ofrece una amplia gama de telas deportivas de alta calidad para indumentaria deportiva, fitness y más. Encuentra las mejores telas en nuestro local de Flores, Buenos Aires.",
     type: "website",
